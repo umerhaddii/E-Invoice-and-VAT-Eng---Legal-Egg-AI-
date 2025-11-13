96e9596c5d72ac55af32a5fc645b49f563bc82c3
Umer Haddii <153385713+umerhaddii@users.noreply.github.com>
2025-11-13 18:13:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_pinecone import PineconeVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
@@ -209,4 +209,5 @@ if prompt := st.chat_input("Ask your question..."):
         response = process_query(prompt)
         st.write(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
 
